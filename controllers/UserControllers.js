0be6dee5ee6d0b249bc8224f46d37dc4fe96801d
tfsiqueira <thiagofelipesiqueira@gmail.com>
2020-02-15 12:22:46 -0300

    Working with Promise

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -14,9 +14,58 @@ class UserController {
 
             event.preventDefault();
 
-            this.addLine(this.getValues());
+            let values = this.getValues();
+
+            this.getPhoto().then(
+                (content) => {
+
+                    values.photo = content;
+                    this.addLine(values);
+
+
+                },
+                (e) => {
+
+                    console.error(e);
+
+                }
+            );
 
         });
+    }
+
+    getPhoto() {
+
+        return new Promise((resolve, reject) => {
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+
+                if (item.name === 'photo') {
+                    return item;
+
+                }
+
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = (e) => {
+
+                reject(e);
+            }
+
+            fileReader.readAsDataURL(file);
+
+        });
+
 
     }
 
@@ -56,7 +105,7 @@ class UserController {
 
         this.tableEl.innerHTML = `
             <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin}</td>
