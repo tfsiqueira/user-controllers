2e6468a2ada278f3bd977c32447d9997c35c1c3b
tfsiqueira <thiagofelipesiqueira@gmail.com>
2020-03-04 21:50:09 -0300

    Button Delete

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -220,7 +220,7 @@ class UserController {
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-xs btn-delete btn-flat">Excluir</button>
             </td>
         `;
 
@@ -233,6 +233,15 @@ class UserController {
     }
 
     addEventTr(tr) {
+
+        tr.querySelector(".btn-delete").addEventListener("click", e => {
+
+            if (confirm("Deseja realmente excluir?")) {
+                tr.remove();
+                this.updateCount();
+            }
+        });
+
         tr.querySelector(".btn-edit").addEventListener("click", e => {
 
             let json = JSON.parse(tr.dataset.user);
